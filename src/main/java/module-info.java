9ae251ba370183f3d44251192a024f3fc1283d0f
alexandra7667 <alexandraharnstrom@Alexandras-MacBook-Pro.local>
2023-01-22 13:22:53 +0100

    first commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
-module com.example.emailsender_3_2_1 {
+module com.example.emailsender {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.mail;
 
 
-    opens com.example.emailsender_3_2_1 to javafx.fxml;
-    exports com.example.emailsender_3_2_1;
+    opens com.example.emailsender to javafx.fxml;
+    exports com.example.emailsender;
 }
